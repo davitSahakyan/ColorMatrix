aa87267d48f6168044e5cf60d4f46cdc1177f5b3
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-08-17 16:46:56 +0400

    Add script.js

---
+++ script.js
@@ -0,0 +1,69 @@
+const div = document.createElement("DIV");
+const rootElement = document.getElementById("root");
+let array = [];
+
+function mouseOver() {
+    matrixElementDiv.style.color = "white";
+    console.log(matrixElementDiv.id);
+}
+
+function mouseOut() {
+    matrixElementDiv.style.color = "transparent";
+}
+
+function buildMatrix() {
+    for (let i = 0; i <= 255; i = i + 10) {
+        array.push(i);
+    }
+    return array;
+}
+buildMatrix();
+
+array.forEach((blueColor) => {
+    const matrixRow = document.createElement("div");
+    array.forEach((greenColor, index) => {
+        const randomId =
+            Math.random().toString(36).substring(2, 15) +
+            Math.random().toString(36).substring(2, 15);
+        const matrixElementDiv = document.createElement("div");
+        const rgbContainerDiv = document.createElement("div");
+        const rgbTextContainer = document.createElement("div");
+
+        const redColorSpan = document.createElement("span");
+        const blueColorSpan = document.createElement("span");
+        const greenColorSpan = document.createElement("span");
+        const rgbTextSpan = document.createElement("span");
+
+        redColorSpan.textContent = `${0}, `;
+        blueColorSpan.textContent = `${blueColor}, `;
+        greenColorSpan.textContent = greenColor;
+        rgbTextSpan.textContent = "RGB";
+
+        rgbContainerDiv.appendChild(redColorSpan);
+        rgbContainerDiv.appendChild(blueColorSpan);
+        rgbContainerDiv.appendChild(greenColorSpan);
+        rgbTextContainer.appendChild(rgbTextSpan);
+
+        matrixElementDiv.style.backgroundColor = `rgb(0, ${blueColor}, ${greenColor})`;
+        matrixRow.classList.add("matrixRow");
+        matrixElementDiv.classList.add("matrixElement");
+        matrixElementDiv.id = randomId;
+        matrixElementDiv.addEventListener("mouseover", mouseOver);
+        matrixElementDiv.addEventListener("mouseout", mouseOut);
+
+        function mouseOver() {
+            matrixElementDiv.style.color = "white";
+            console.log(matrixElementDiv.id);
+        }
+
+        function mouseOut() {
+            matrixElementDiv.style.color = "transparent";
+        }
+
+        matrixElementDiv.appendChild(rgbTextContainer);
+        matrixElementDiv.appendChild(rgbContainerDiv);
+        matrixRow.appendChild(matrixElementDiv);
+        rootElement.appendChild(matrixRow);
+    });
+    rootElement.appendChild(matrixRow);
+});
