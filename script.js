665a13210037d31ff195eee3b7ebba5bf6714148
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-08-21 16:23:39 +0400

    Refactor project

--- script.js
+++ script.js
@@ -3,47 +3,29 @@ const rootElement = document.getElementById("root");
 
 function mouseOver(matrixElementDiv) {
     matrixElementDiv.style.color = "white";
-    console.log(matrixElementDiv.id);
 }
 
 function mouseOut(matrixElementDiv) {
     matrixElementDiv.style.color = "transparent";
 }
 
-function buildMatrix() {
-    for (let blueColor = 0; blueColor <= 255; blueColor = blueColor + 1) {
+async function buildMatrix() {
+    for (let blueColor = 0; blueColor <= 255; blueColor++) {
         const matrixRow = document.createElement("div");
-        for (
-            let greenColor = 0;
-            greenColor <= 255;
-            greenColor = greenColor + 1
-        ) {
-            const randomId =
-                Math.random().toString(36).substring(2, 15) +
-                Math.random().toString(36).substring(2, 15);
+        for (let greenColor = 0; greenColor <= 255; greenColor++) {
             const matrixElementDiv = document.createElement("div");
             const rgbContainerDiv = document.createElement("div");
             const rgbTextContainer = document.createElement("div");
+            const indexSpan = document.createElement("span");
+            indexSpan.textContent = `M(${blueColor} ${greenColor})`;
 
-            const redColorSpan = document.createElement("span");
-            const blueColorSpan = document.createElement("span");
-            const greenColorSpan = document.createElement("span");
-            const rgbTextSpan = document.createElement("span");
-
-            redColorSpan.textContent = `${0}, `;
-            blueColorSpan.textContent = `${blueColor}, `;
-            greenColorSpan.textContent = greenColor;
-            rgbTextSpan.textContent = "RGB";
-
-            rgbContainerDiv.appendChild(redColorSpan);
-            rgbContainerDiv.appendChild(blueColorSpan);
-            rgbContainerDiv.appendChild(greenColorSpan);
-            rgbTextContainer.appendChild(rgbTextSpan);
+            rgbContainerDiv.textContent = `(${0} ${blueColor} ${greenColor})`;
+            rgbTextContainer.textContent = "RGB";
 
             matrixElementDiv.style.backgroundColor = `rgb(0, ${blueColor}, ${greenColor})`;
             matrixRow.classList.add("matrixRow");
             matrixElementDiv.classList.add("matrixElement");
-            matrixElementDiv.id = randomId;
+            matrixElementDiv.id = `M(${blueColor} ${greenColor})`;
 
             matrixElementDiv.addEventListener("mouseover", () =>
                 mouseOver(matrixElementDiv)
@@ -54,6 +36,7 @@ function buildMatrix() {
 
             matrixElementDiv.appendChild(rgbTextContainer);
             matrixElementDiv.appendChild(rgbContainerDiv);
+            matrixElementDiv.appendChild(indexSpan);
             matrixRow.appendChild(matrixElementDiv);
             rootElement.appendChild(matrixRow);
         }
